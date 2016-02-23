62e447ff91ce2df382c8e7c44779ebbfce65efe1
Kaylan Smith <kaylansmith@Kaylans-MBP.attlocal.net>
2016-02-22 23:37:58 -0600

    all pieces there, some small tweaks to be made

--- js/script.js
+++ js/script.js
@@ -8,14 +8,13 @@ var startClock = function() {
     var m = today.getMinutes()
     // converts current minutes into hex value, base 16
     var minutes = m.toString(16)
-    console.log(minutes)
     var s = today.getSeconds()
     // converts current seconds into hex value, base 16
     var seconds = s.toString(16)
     // runs helper function checkTime to add 0 to hours if needed
     m = checkTime(m)
     s = checkTime(s)
-    // 
+    // creates time string from current hours, minutes and seconds
     document.querySelector("#clock").innerHTML = h + ":" + m + ":" + s
     setTimeout(startClock, 500)
     //select overlay
@@ -37,4 +36,17 @@ var checkTime = function(i) {
     return i
 }
 
+// line grow
+var line = document.querySelector("#line")
+    width = 0
 
+
+// increase width of line 10px
+var timer = setInterval(function() {
+  line.style.width = ( width += 10 ) + "px"
+  // at 350px, line shrinks back to 0
+  if ( width === 350 ) {
+    width = 0
+    // clearInterval( timer )
+  }
+}, 350)  
